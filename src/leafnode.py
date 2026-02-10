f475e178513d91f15b82bf705f342def06af6754
Oliwier Kniazewski <oliwierkniazewski@gmail.com>
2026-02-10 18:41:57 +0100

    add leafnode with tests

---
+++ src/leafnode.py
@@ -0,0 +1,22 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str|None = None, value: str|None = None,
+                props: dict|None = None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        
+        if self.tag == None:
+            return self.value
+        else:
+            html_attrs: str = self.props_to_html()
+            print(self.props)
+            print(f"html_attrs: {html_attrs}")
+            return f"<{self.tag}{html_attrs}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode(tag = {self.tag}, value = {self.value}, props = {self.props})"
+    
---
+++ src/test_leafnode.py
@@ -0,0 +1,18 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        
+    def test_leaf_with_prop_html(self):
+        node = LeafNode("a", "Whazzup", {"a": "b", "c": "d"})
+        self.assertEqual(node.to_html(), '<a a="b" c="d">Whazzup</a>')
+
+
+
+
+
+    
